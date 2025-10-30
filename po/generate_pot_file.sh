8ce6d755e3cc9c2b7a7e97ae2d6ca7203af300cd
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-10-30 17:52:52 +0300

    added a localization struct

---
+++ po/generate_pot_file.sh
@@ -0,0 +1,2 @@
+cd $(dirname "$0")
+xgettext --files-from=files --language=Python -o hakki-text-editor.pot
--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def on_activate(app):
 		win = MainWindow(app)
 	win.present()
 
-app = Gtk.Application(application_id='me.ihakkiergin.notepad')
+app = Gtk.Application(application_id='me.ihakkiergin.hakkitor')
 app.connect('activate', on_activate)
 
 app.set_accels_for_action('win.new', ['<Ctrl>t'])
